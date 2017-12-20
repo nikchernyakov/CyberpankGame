e5457d6c43fe589f17b5ac59de06f64a055e1906
Nikita Chernyakov <nikchernyakov@yandex.ru>
2017-12-20 13:44:50 +0300

    Update
    - Delete useless actions and values

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -8,8 +8,6 @@ public class Bullet : Damageble {
     public LayerMask whatIsTarget;
 
     private Rigidbody2D rb;
-    private Vector2 direction;
-    private float bulletSpeed;
     private Vector2 bulletVelocity;
 
     // Use this for initialization
@@ -24,25 +22,18 @@ public class Bullet : Damageble {
 	
 	// Update is called once per frame
 	void Update () {
-        if(bulletLifeTime < 0)
+        //Debug.Log(rb.velocity);
+        if (bulletLifeTime < 0)
         {
             Die();
         }
 
-        if (direction == null) return;
         bulletLifeTime -= Time.deltaTime;
 	}
 
     public void SetVelocity(Vector2 velocity)
     {
         bulletVelocity = velocity;
-        if(rb != null)
-            rb.velocity = bulletVelocity;
-    }
-
-    public void SetSpeed(float speed)
-    {
-        bulletSpeed = speed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
