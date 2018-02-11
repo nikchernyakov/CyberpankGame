3c7714be00311a94811129f64c0dd550f1329994
Nikita Chernyakov <nikchernyakov@yandex.ru>
2018-02-11 23:35:58 +0300

    Add SpeedController
    To fix bug with fall through bounds

---
+++ Assets/Scripts/Handlers/SpeedController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour {
+
+    public float minSpeedValue = 0f;
+    public float maxSpeedValue = 0f;
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, minSpeedValue, maxSpeedValue));
+	}
+}
