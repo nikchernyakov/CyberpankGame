d55f121a5c5946deb863a167a45a682cee0d83fc
nemestniy <tushkevich.ma@gmail.com>
2018-05-16 20:00:05 +0300

    Add - GameObjectGenerate - Script for generating of objects with list of places

---
+++ Assets/Scripts/GameLifecycle/Services/GameObjectsGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectsGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject objectForGenerate;
+
+    [SerializeField]
+    private List<Transform> placesForGenerate;
+
+    public void Generate()
+    {
+        foreach (Transform place in placesForGenerate)
+            Instantiate(objectForGenerate, place);
+    }
+}
