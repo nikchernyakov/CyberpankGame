41461ba08477f8716bffbfa5d6e0ef04fb1d9481
Max Barashev <33381533+nemestniy@users.noreply.github.com>
2018-05-21 12:01:26 +0300

    Test branch
    * Initialization
    
    * no message
    
    * Add - GameObjectGenerate - Script for generating of objects with list of places
    
    * Fix - GameObjectsGenerator - Change List of Transform on Transform

---
+++ Assets/Scripts/GameLifecycle/Services/GameObjectsGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectsGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject objectForGenerate;
+
+    [SerializeField]
+    private Transform placeForGenerate;
+
+    public void Generate()
+    {
+            Instantiate(objectForGenerate, placeForGenerate);
+    }
+}
