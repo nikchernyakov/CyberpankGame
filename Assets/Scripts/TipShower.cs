bf0ddf3b1a2fc1c14649a3e5649f8eeae81f8dd2
nemestniy <tushkevich.ma@gmail.com>
2018-07-24 01:57:26 +0300

    SCR-1:Create ZoneEnterEvent script and TipShower script. Create collider ZoneEnterEvent and new scprite - tip.

---
+++ Assets/Scripts/TipShower.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipShower : ObjectShower {
+    [SerializeField]
+    private float timer;
+
+    private float a;
+    private bool isActive = false;
+
+    private void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            if (!isActive)
+            {
+                a = gameObject.GetComponent<SpriteRenderer>().color.a;
+                a += Time.deltaTime;
+                gameObject.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, a);
+                if (a >= 1)
+                    isActive = true;
+            }
+
+            if (isActive)
+            {
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    a -= Time.deltaTime;
+                    gameObject.GetComponent<SpriteRenderer>().color = new Vector4(1, 1, 1, a);
+                    if (a <= 0)
+                        gameObject.SetActive(false);
+                }
+            }
+        }
+            
+    }
+}
---
+++ Assets/Scripts/ZoneEnterEvent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneEnterEvent : Doneble
+{
+    [SerializeField]
+    private ObjectShower tipShower;
+
+    public bool isDone = false;
+
+    public override bool IsDone()
+    {
+        return isDone;
+    }
+
+    protected override void ChangeDone()
+    {
+        isDone = (IsDone()) ? false : true;
+        if (IsDone())
+        {
+            tipShower.UpdateDone(this);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "ZoneEnterEvent")
+        {
+            ChangeDone();
+            Debug.Log("enter");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Tip")
+        {
+            ChangeDone();
+            Debug.Log("exit");
+        }
+    }
+}
